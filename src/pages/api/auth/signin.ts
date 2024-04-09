f51093c7ded29c4ec88dd93ba7a2a2ac2805e8fa
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-04-09 03:58:22 -0400

    Update signin.ts

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -14,8 +14,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
       options: {
         redirectTo: import.meta.env.DEV
           ? "http://localhost:4321/api/auth/callback"
-          : "https://astro-supabase-castell-p7biy2m3j-castelll0009s-projects.vercel.app/api/auth/callback",
-          // : "https://lucky-meringue-8b3000.netlify.app/api/auth/callback"
+          : "https://astro-supabase-castell-p7biy2m3j-castelll0009s-projects.vercel.app/api/auth/callback",       
       },
     });
 
