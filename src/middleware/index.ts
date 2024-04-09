92f5874f1f67dfb829c9d2a8aa21415e3e66dd84
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-04-09 05:34:38 -0400

    Update index.ts

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 import { supabase } from "../lib/supabase";
 import micromatch from "micromatch";
 
-const protectedRoutes = ["/dashboard(|/)"];
+const protectedRoutes = [""];
 const redirectRoutes = ["/signin(|/)", "/register(|/)"];
 const proptectedAPIRoutes = ["/api/guestbook(|/)"];
 
