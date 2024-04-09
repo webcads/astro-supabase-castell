f4e3c2603e8f6006fab7ebe1975f5380abe3a042
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-04-09 03:05:59 -0400

    comente esta mierda y lo puse local
    // autoRefreshToken: false,
          // detectSessionInUrl: false,
          // persistSession: true,

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -9,6 +9,7 @@ export const supabase = createClient(
       // autoRefreshToken: false,
       // detectSessionInUrl: false,
       // persistSession: true,
+      //comente esta mierda y lo puse local
     },
   },
 );
