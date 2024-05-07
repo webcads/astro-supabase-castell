3a8c98af445f5aa4b3c01c8c81b4401be23af6e7
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-05-07 08:10:04 -0400

    error in request redirect

--- src/pages/api/carnet.ts
+++ src/pages/api/carnet.ts
@@ -2,38 +2,38 @@ import type { APIRoute } from "astro";
 import { supabase } from "../../lib/supabase";
 
 export const POST: APIRoute = async ({ request, redirect }) => {
-  // Obtener los datos del formulario del cuerpo de la solicitud
-  const { email, rh, foto } = await request.json();
+  // // Obtener los datos del formulario del cuerpo de la solicitud
+  // const { email, rh, foto } = await request.json();
 
   // Verificar si el correo electrónico del usuario está presente
-  if (!email) {
-    return redirect("/email no encontrado");
-  }
+  // if (!email) {
+  //   return redirect("/email no encontrado");
+  // }
 
-  // Verificar si el usuario ya existe en la tabla de usuarios
-  let { data: existingUser, error: searchError } = await supabase
-    .from("usuario")
-    .select("*")
-    .eq("correo_unicauca", email)
-    .single();
+  // // Verificar si el usuario ya existe en la tabla de usuarios
+  // let { data: existingUser, error: searchError } = await supabase
+  //   .from("usuario")
+  //   .select("*")
+  //   .eq("correo_unicauca", email)
+  //   .single();
 
-  if (searchError) {
-    return redirect("/error al buscar usuario");
-  }
+  // if (searchError) {
+  //   return redirect("/error al buscar usuario");
+  // }
 
 
 
-  // Insertar los datos del carné en la tabla de carnés
-  const { data: carnetData, error: carnetInsertError } = await supabase
-    .from("carnet")
-    .insert([{ usuario_id: existingUser.id, rh, foto }]);
+  // // Insertar los datos del carné en la tabla de carnés
+  // const { data: carnetData, error: carnetInsertError } = await supabase
+  //   .from("carnet")
+  //   .insert([{ usuario_id: existingUser.id, rh, foto }]);
 
-  if (carnetInsertError) {
-    return new Response(
-      JSON.stringify({ error: "Error al insertar datos del carné" }),
-      { status: 500 }
-    );
-  }
+  // if (carnetInsertError) {
+  //   return new Response(
+  //     JSON.stringify({ error: "Error al insertar datos del carné" }),
+  //     { status: 500 }
+  //   );
+  // }
 
   // Redirigir al usuario a la página de carné digital
   return redirect("/carnetdigital");
